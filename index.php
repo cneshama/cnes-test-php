9cb36630669aa247708365c0a6caef18254f734d
root <root@master.localdomain>
2017-06-19 14:29:01 +0900

    mariadb connect test

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php echo "CNES OpenShift Test rev2 [DB Connect]"; ?>
+<?php echo "CNES OpenShift Test rev2 [DB Connect]";
 $mysqli = new mysqli(getenv("MARIADB_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
 if ($mysqli->connect_error) {
     exit($mysqli->connect_error);
