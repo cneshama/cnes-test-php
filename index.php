232674db80609f7b336776c32c1912ac99b22442
root <root@master.localdomain>
2017-06-19 13:42:22 +0900

    mariadb connect test

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php echo "CNES OpenShift Test rev2"; ?>
+$mysqli = new mysqli(getenv("MARIADB_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+if ($mysqli->connect_error) {
+    exit($mysqli->connect_error);
+} else {
+    $mysqli->set_charset("utf8");
+}
+$result = $mysqli->query("select 1") or exit($mysqli->error());
+echo var_dump($result->fetch_assoc());
+$mysqli->close();
+
+?>
